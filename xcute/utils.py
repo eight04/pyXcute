9867c234a17adf0c458e36f917e30017ca24381e
eight <eight04@gmail.com>
2017-03-28 01:55:26 +0800

    Refactor. Drop docopt

--- xcute/utils.py
+++ xcute/utils.py
@@ -1,84 +1,106 @@
 #! python3
 
-from docopt import docopt
-from os import makedirs
-from pathlib import Path
-from send2trash import send2trash
-from shutil import copy2
-import sys
-from natsort import natsorted
+def iter_files(src, patterns, ignores=None, ordered_patterns=False,
+		no_subdir=False, no_dir=False):
+		
+	from ordered_set import OrderedSet
+	from natsort import natsorted
+	
+	processed = OrderedSet()
 
-def clean():
-	doc = """Delete folders and files.
-	
-Usage:
-  x-clean [--source=<dir>] <pattern>...
-  
-Options:
-  -s --source=<dir>  Source directory. [default: .]
-"""
-	args = docopt(doc)
-	src = Path(args["--source"])
-	for p in args["<pattern>"]:
-		for file in src.glob(p):
-			print("delete", file)
-			send2trash(str(file))
-	
-def concat():
-	doc = """Concat files and print to stdout. Each file is only printed once.
-	
-Usage:
-  x-cat [--source=<dir>] [--no-line-end] [--ignore=<pattern>]...
-        <pattern>...
-  
-Options:
-  -s --source=<dir>      Source directory. [default: .]
-  -i --ignore=<pattern>  Ignore file pattern.
-  -o --no-line-end       Don't print line end after each file.
-"""	
-	args = docopt(doc)
-	src = Path(args["--source"])
-	ignore = args["--ignore"]
-	end = "" if args["--no-line-end"] else "\n"
-	processed = set()
-	for p in args["<pattern>"]:
-		for file in natsorted(src.glob(p)):
+	def _iter_files(files):
+		for file in natsorted(files):
+			if no_subdir and processed and processed[-1] in file.parents:
+				continue
+			if ignores and any(file.match(i) for i in ignores):
+				continue
+			if no_dir and file.is_dir():
+				continue
 			if file in processed:
 				continue
 			processed.add(file)
-			if any(file.match(i) for i in ignore):
-				continue
-			print(file.read_text("utf-8"), end=end)
+			yield file
+			
+	if ordered_patterns:
+		# remain pattern's order
+		for pattern in patterns:
+			yield from _iter_files(src.glob(pattern))
+	else:
+		yield from _iter_files((f for p in patterns for f in src.glob(p)))
+
+	if not processed:
+		print("No file is matched.")
+
+def clean():
+	from pathlib import Path
+	from argparse import ArgumentParser
+	from send2trash import send2trash
+	
+	parser = ArgumentParser(description="Delete folders and files.")
+	parser.add_argument(
+		"-s", dest="src", metavar="<src_dir>", default=".", type=Path,
+		help="Source directory.")
+	parser.add_argument(
+		"patterns", metavar="<pattern>", nargs="+",
+		help="Glob pattern matching files/folders.")
+	args = parser.parse_args()
+	
+	for file in iter_files(args.src, args.patterns, no_subdir=True):
+		print("delete", file)
+		send2trash(str(file))
+	
+def concat():
+	from argparse import ArgumentParser
+	from pathlib import Path
+	
+	parser = ArgumentParser(
+		description="Concat files and print to stdout. Each file is only"
+			" printed once.")
+	parser.add_argument(
+		"-s", dest="src", metavar="<src_dir>", default=".", type=Path,
+		help="Source directory.")
+	parser.add_argument(
+		"patterns", metavar="<pattern>", nargs="+",
+		help="Glob pattern matching files/folders.")
+	parser.add_argument(
+		"-i", dest="ignores", metavar="<pattern>", nargs="+",
+		help="Glob pattern matching files/folders to be ignored.")
+	parser.add_argument(
+		"-n", dest="end", action="store_const", const="", default="\n",
+		help="Don't print line end after each file.")
+	args = parser.parse_args()
+	
+	for file in iter_files(args.src, args.patterns, ignores=args.ignores,
+			ordered_patterns=True, no_dir=True):
+		print(file.read_text("utf-8"), end=args.end)
 	
 def copy():
-	doc = """Copy files. Note that a if pattern matches a folder, the folder
-is ignored. Use **/* to copy folder's content.
-
-Usage:
-  x-copy [--source=<dir] [--ignore=<pattern>]... <dest_dir> <pattern>...
-  
-Options:
-  -s --source=<dir>      Source directory. [default: .]
-  -i --ignore=<pattern>  Ignore file pattern.
-"""
-	args = docopt(doc)
-	src = Path(args["--source"])
-	dest = Path(args["<dest_dir>"])
-	ignore = args["--ignore"]
-	files = set()
-	for pattern in args["<pattern>"]:
-		files.update(src.glob(pattern))
-	if not files:
-		print("Patterns doesn't match any file.")
-		return
+	from argparse import ArgumentParser
+	from pathlib import Path
+	
+	parser = ArgumentParser(
+		description="Copy files. Note that a if pattern matches a folder, the"
+			" folder is ignored. Use **/* to copy folder's content.")
+	parser.add_argument(
+		"-s", dest="src", metavar="<src_dir>", default=".", type=Path,
+		help="Source directory.")
+	parser.add_argument(
+		"-i", dest="ignores", metavar="<pattern>", nargs="+",
+		help="Glob pattern matching files/folders to be ignored.")
+	parser.add_argument(
+		"dest", metavar="<dest_dir>", type=Path, help="Destination directory.")
+	parser.add_argument(
+		"patterns", metavar="<pattern>", nargs="+",
+		help="Glob pattern matching files/folders.")
+	args = parser.parse_args()
+	
+	from shutil import copy2
+	from os import makedirs
 	
 	created_parents = set()
-	for file in files:
-		if file.is_dir():
-			continue
-		if any(file.match(i) for i in ignore):
-			continue
-		new_file = dest / file.relative_to(src)
+	for file in iter_files(args.src, args.patterns, ignores=args.ignores,
+			no_dir=True):
+		new_file = args.dest / file.relative_to(args.src)
 		print("copy", file)
 		if new_file.parent not in created_parents:
 			makedirs(new_file.parent, exist_ok=True)
@@ -86,14 +108,19 @@ Options:
 		copy2(file, new_file)
 	
 def pipe():
-	doc = """Read stdin and output to a file. It automatically creates missing
-folders.
+	from argparse import ArgumentParser
+	from pathlib import Path
 
-Usage:
-  x-pipe <file>
-"""
-	args = docopt(doc)
-	with open(args["<file>"], "w") as f:
+	parser = ArgumentParser(
+		description="Read stdin and output to a file. It automatically creates"
+			" missing folders.")
+	parser.add_argument(
+		"dest", metavar="<dest_file>", type=Path,
+		help="Destination file.")
+	args = parser.parse_args()
+		
+	import sys
+	
+	with args.dest.open("w") as f:
 		for line in sys.stdin:
 			f.write(line)
-	
