909916e49352e94198e1f8b970d1d4cda268e2d9
eight <eight04@gmail.com>
2018-05-11 14:59:12 +0800

    Fix: convert args into string

--- xcute/__init__.py
+++ xcute/__init__.py
@@ -153,7 +153,7 @@ class Cmd:
         for cmd in self.cmds:
             args = shlex.split(f(cmd)) + list(args)
             log("> Cmd: {}".format(" ".join(args)))
-            subprocess.run(args, shell=sys.platform == "win32", check=True)
+            subprocess.run(subprocess.list2cmdline(args), shell=True, check=True)
         
 class Bump:
     """An executor which can bump the version inside a .py file."""
